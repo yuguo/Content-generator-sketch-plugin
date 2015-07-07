cb7748fdfadfbee4f229fc6eeb339d48474946aa
yuguo <fishguo321@gmail.com>
2015-07-07 17:26:19 +0800

    Fit Chinese names

--- js/loadNames.js
+++ js/loadNames.js
@@ -3,7 +3,7 @@ var loadText = function(data, secondData){
 		var layer = selection[i];
 		var randomName = data[ Math.floor(Math.random() * data.length) ];
 
-		if(secondData) randomName += ' ' + secondData[ Math.floor(Math.random() * secondData.length) ];
+		if(secondData) randomName += secondData[ Math.floor(Math.random() * secondData.length) ] + (Math.random() < 0.5 ? secondData[ Math.floor(Math.random() * secondData.length)] : '') ;
 
 		[layer setStringValue: randomName];
 		[layer setName: randomName];
